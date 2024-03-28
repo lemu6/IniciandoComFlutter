0f05c1b40f13ba05588c781c67850efd7321d085
LemuDev <lemu200011@gmail.com>
2024-03-27 23:48:02 -0300

    aula 36 daves

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  int valor = 10;
-  runApp(MyApp(title: 'Aplicativo ola mundo title', valor: valor));
-  valor++;
+void main(){
+
+
 }
 
-class MyApp extends StatelessWidget {
-  final String title;
-  final int valor;
-
-  const MyApp({Key? key, this.title = '', this.valor = 0}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key : key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(this.title),
-        ),
-        body: Center(
-          child: Text(
-            'Ola mundo, valor é ' + this.valor.toString(),
-            style: TextStyle(fontSize: 50, color: Colors.black),
-          ),
-        ),
-      ),
-    );
+    return Container();
+    child: child
   }
 }
